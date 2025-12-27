dd8b8454688ce9ac2640ca1bdf7370c22861d276
Rosmeowtis <rosmeowtis@outlook.com>
2025-12-27 16:07:22 +0800

    chore: 清理无用依赖

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -3,11 +3,9 @@
 //! 提供通用辅助函数，如文件系统操作、正则表达式模式等。
 
 mod fs;
-mod regex_patterns;
 mod token_estimator;
 mod logger;
 
 pub use fs::*;
-pub use regex_patterns::*;
 pub use token_estimator::*;
 pub use logger::*;
